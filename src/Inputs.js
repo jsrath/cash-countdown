6c7c888efa82f109981e03e73846730c4e3b3694
Jeremy <jeremy@jsrath.com>
2018-11-02 16:23:20 +0100

    Created income input and updated styles

--- src/Inputs.js
+++ src/Inputs.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Card, Container, Fa, Row } from 'mdbreact';
+import { Card, Container, Fa, Row } from 'mdbreact';
 import { LineChart } from 'react-d3-components';
 
 class Inputs extends Component {
@@ -11,6 +11,7 @@ class Inputs extends Component {
       exiry: '',
       total: '',
       spend: '',
+      income: '',
       graph: false,
       data: [{ key: 'Cash', values: [] }],
     };
@@ -22,8 +23,8 @@ class Inputs extends Component {
   };
 
   updateTime = () => {
-    if (this.state.total && this.state.spend) {
-      const months = Math.round((this.state.total / this.state.spend) * 10) / 10;
+    if (this.state.total && this.state.spend && this.state.income) {
+      const months = Math.round((this.state.total / (this.state.spend - this.state.income)) * 10) / 10;
       this.setState({ monthsLeft: months });
       this.setState({ daysLeft: months * 30 }, () => this.updateExpiry());
     }
@@ -39,6 +40,7 @@ class Inputs extends Component {
   };
 
   updateTotal = event => {
+    event.target.value = Number(event.target.value.split(',').join(''));
     if (!isNaN(event.target.value)) {
       const numeric = Number(event.target.value);
       this.setState({ total: numeric }, () => this.updateTime());
@@ -46,9 +48,18 @@ class Inputs extends Component {
   };
 
   updateSpend = event => {
+    event.target.value = Number(event.target.value.split(',').join(''));
     if (!isNaN(event.target.value)) {
       const numeric = Number(event.target.value);
       this.setState({ spend: numeric }, () => this.updateTime());
+    }
+  };
+
+  updateIncome = event => {
+    event.target.value = Number(event.target.value.split(',').join(''));
+    if (!isNaN(event.target.value)) {
+      const numeric = Number(event.target.value);
+      this.setState({ income: numeric }, () => this.updateTime());
     }
   };
 
@@ -89,7 +100,7 @@ class Inputs extends Component {
               Cash Countdown
               <Fa className="title-icon" icon="money" />
             </h3>
-            <span className="heading">
+            <span className="heading heading-inputs">
               <h5 className="d-inline w-25">Inputs</h5>
             </span>
             <Row className="mt-4">
@@ -97,19 +108,26 @@ class Inputs extends Component {
                 <p className="input-label">Current Cash on Hand</p>
                 <div className="input-container">
                   <Fa icon="money" />
-                  <input className="my-2 w-100" type="text" value={this.state.total} onChange={this.updateTotal} />
+                  <input className="my-2 w-100" type="text" value={this.state.total.toLocaleString()} onChange={this.updateTotal} />
                 </div>
               </div>
               <div className="col-sm-4 spend">
                 <p className="input-label">Monthly Spending</p>
                 <div className="input-container">
                   <Fa icon="share-square-o" />
-                  <input className="my-2 w-100" type="text" value={this.state.spend} onChange={this.updateSpend} />
+                  <input className="my-2 w-100" type="text" value={this.state.spend.toLocaleString()} onChange={this.updateSpend} />
+                </div>
+              </div>
+              <div className="col-sm-4 income">
+                <p className="input-label">Monthly Income</p>
+                <div className="input-container">
+                  <Fa icon="sign-in" />
+                  <input className="my-2 w-100" type="text" value={this.state.income.toLocaleString()} onChange={this.updateIncome} />
                 </div>
               </div>
             </Row>
 
-            <span className="heading">
+            <span className="heading result-color">
               <h5 className="d-inline w-25">Results</h5>
             </span>
 
@@ -117,27 +135,30 @@ class Inputs extends Component {
               <div className="col-sm-4">
                 <p className="result-label">Months Left</p>
                 <div className="input-container">
-                  <Fa icon="calendar" />
+                  <Fa icon="calendar" className="result-color" />
                   <p className="result-box w-100">{this.state.monthsLeft}</p>
                 </div>
               </div>
               <div className="col-sm-4">
                 <p className="result-label">Days Left</p>
                 <div className="input-container">
-                  <Fa icon="calendar-o" />
+                  <Fa icon="calendar-o" className="result-color" />
                   <p className="result-box w-100">{this.state.daysLeft}</p>
                 </div>
               </div>
               <div className="col-sm-4">
                 <p className="result-label">Cash Runs Out</p>
                 <div className="input-container">
-                  <Fa icon="calendar-times-o" />
+                  <Fa icon="calendar-times-o" className="result-color" />
                   <p className="result-box w-100">{this.state.expiry}</p>
                 </div>
               </div>
             </Row>
-            <span onClick={this.showGraph} className="heading clickable mb-5">
-              <h5 className="d-inline w-25">Show Graph</h5>
+            <span className="heading graph-color mb-5">
+              <h5 className="d-inline w-25">Graph</h5>
+            </span>
+            <span onClick={this.showGraph} className="graph-button">
+              Show Graph
             </span>
             {this.state.graph ? (
               <div className="chart w-100">
@@ -151,6 +172,7 @@ class Inputs extends Component {
                   xAxis={{ innerTickSize: 6, label: 'Time' }}
                   yAxis={{ label: 'Cash' }}
                   stroke={{ strokeDasharray: '5', strokeWidth: '2' }}
+                  shapeColor="#c5cae9"
                 />
               </div>
             ) : null}
